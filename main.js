aef18d70252c9df59002de1ae33685d5230ef181
Ricardo Jeferson da Silva Francisco <122450316+totorourbem@users.noreply.github.com>
2023-12-18 10:47:48 -0300

    Criada branch
    Branch à parte para adequações à Sprint 3

---
+++ main.js
@@ -0,0 +1,8 @@
+// Botão de menu #menusvg
+
+//id do menu de pesisquisa no site #pesquisa
+
+//class dos incones de media 'social-icon' [implementar]
+
+//class dos menus class="menu-item" [implementar]
+

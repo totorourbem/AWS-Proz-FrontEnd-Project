28bf7fc06793493144a122967898f9d0d4afe831
Ricardo Jeferson da Silva Francisco <122450316+totorourbem@users.noreply.github.com>
2024-02-14 21:03:36 -0300

    Adaptação do site ao Bootstrap
    Adequação ao site ao botstrap, porem, sem javascript.

--- main.js
+++ main.js
@@ -1,52 +1,184 @@
-// Forçar a captura do DOM
+// Variável global de dados
 
-document.addEventListener("DOMContentLoaded", function() {
-   
-    // Botão de menu principal #menubtn in menu-button
-    // Menu principal em burger-menu-open
-    // Conteudo do site em content-menu-open
-    // Botão de menu login #loginbtn in social-dock
-    // Menu login em  em modal-login-close
-    // Variáveis - captura de elementos
-
-    const menubtn = document.querySelector('#menubtn');
-    const mainmenu = document.querySelector('#burger-menu-open');
-    const container = document.querySelector('#content-menu-open');
-    const loginbtn = document.querySelector('#loginbtn');
-    const loginmenu = document.querySelector('#modal-login-close');
-    const closeloginbtn =document.querySelector('#close-modal')
-    ;
-    // Adicionar os 'event listener' para o botão de menu
-
-    menubtn.addEventListener('click', () => {
-        //Pega o id do menu e o fecha, coordenadamente com o menu
-        if (mainmenu.id == 'burger-menu-open' && container.id == 'content-menu-open') {
-            //Atribui novo valor
-            mainmenu.id = 'burger-menu-closed';
-            container.id = 'content-menu-closed';
-        } else {
-            // Atribui valor padrão
-            mainmenu.id= 'burger-menu-open';
-            container.id = 'content-menu-open';
-        }
-    });
-
-    // Adiconar o listener par o icone de login
-
-    loginbtn.addEventListener('click', () => {
-        //Pega o id do menu e o abre a tela de login
-        if (loginmenu.id == 'modal-login-close') {
-            //Atribui novo valor
-            loginmenu.id = 'modal-login-open';
-        } 
-    
-    //pega o botão fechar do menu e volta à pagina
-    
-    closeloginbtn.addEventListener('click', () => {
-        
-        if (loginmenu.id = 'modal-login-open') {
-            loginmenu.id = 'modal-login-close';
-        }
-    });
-});
-})
+const dados = [
+    {
+        avatar: "./Assets/colab7.jpg",
+        email: "cardosomilena@gmail.com",
+        especialidade: "Pintura",
+        fone: "43919787507",
+        nome: "Ana Clara Fernandes",
+        nota_media: "3.272727272727273",
+        prestador_id: "1"
+      },
+      {
+        avatar: "./Assets/Colab1.jpg",
+        email: "cauemelo@yahoo.com.br",
+        especialidade: "Hidráulica",
+        fone: "43968478162",
+        nome: "Calebe Gonçalves",
+        nota_media: "3.142857142857143",
+        prestador_id: "2"
+      },
+      {
+        avatar: "./Assets/colab4.jpg",
+        email: "ana-clarapinto@bol.com.br",
+        especialidade: "Pintura",
+        fone: "43986714076",
+        nome: "Cauê Lopes",
+        nota_media: "3",
+        prestador_id: "3"
+      },
+      {
+        avatar: "./Assets/colab8.jpg",
+        email: "ida-rocha@gmail.com",
+        especialidade: "Alvenaria e infraestrutra",
+        fone: "43935360966",
+        nome: "Vitor Santos",
+        nota_media: "2.8461538461538463",
+        prestador_id: "4"
+      },
+      {
+        avatar: "./Assets/colab4.jpg",
+        email: "silvamilena@gmail.com",
+        especialidade: "Hidráulica",
+        fone: "43920072939",
+        nome: "João Pedro Cavalcanti",
+        nota_media: "3",
+        prestador_id: "5"
+      },
+      {
+        avatar: "./Assets/colab6.jpg",
+        email: "pteixeira@uol.com.br",
+        especialidade: "Hidráulica",
+        fone: "43967547892",
+        nome: "Daniela Fogaça",
+        nota_media: "2.9047619047619047",
+        prestador_id: "6"
+      },
+      {
+        avatar: "./Assets/colab7.jpg",
+        email: "joao-gabriel93@ig.com.br",
+        especialidade: "Elétrica",
+        fone: "43997771995",
+        nome: "Emanuelly Martins",
+        nota_media: "3.5",
+        prestador_id: "7"
+      },
+      {
+        avatar: "./Assets/colab3.jpg",
+        email: "rezendeemanuella@hotmail.com",
+        especialidade: "Pintura",
+        fone: "43922632439",
+        nome: "Maria Alice Rodrigues",
+        nota_media: "2.6153846153846154",
+        prestador_id: "8"
+      },
+      {
+        avatar: "./Assets/colab6.jpg",
+        email: "fernandaoliveira@hotmail.com",
+        especialidade: "Carpintaria",
+        fone: "43958880286",
+        nome: "Amanda da Mata",
+        nota_media: "2.4",
+        prestador_id: "9"
+      },
+      {
+        avatar: "./Assets/colab2.jpg",
+        email: "dcardoso@ig.com.br",
+        especialidade: "Elétrica",
+        fone: "43957977364",
+        nome: "Ana Clara Santos",
+        nota_media: "2.466666666666667",
+        prestador_id: "10"
+      },
+      {
+        avatar: "./Assets/colab3.jpg",
+        email: "diasthiago@hotmail.com",
+        especialidade: "Pintura",
+        fone: "43998258513",
+        nome: "Clarice da Mata",
+        nota_media: "2.7222222222222223",
+        prestador_id: "11"
+      },
+      {
+        avatar: "./Assets/colab5.jpg",
+        email: "lcostela@bol.com.br",
+        especialidade: "Pequenos Reparos",
+        fone: "43929943810",
+        nome: "João Guilherme Ferreira",
+        nota_media: "2.5833333333333335",
+        prestador_id: "12"
+      },
+      {
+        avatar: "./Assets/colab7.jpg",
+        email: "da-motaenzo@yahoo.com.br",
+        especialidade: "Hidráulica",
+        fone:  "43942325716",
+        nome: "Bárbara da Mota",
+        nota_media: "2.7857142857142856",
+        prestador_id: "13"
+      },
+      {
+        avatar: "./Assets/colab2.jpg",
+        email: "43983546328",
+        especialidade: "Pintura",
+        fone:  "43983546328",
+        nome: "Melissa Alves",
+        nota_media: "3.3333333333333335",
+        prestador_id: "14"
+      },
+      {
+        avatar: "./Assets/Colab1.jpg",
+        email: "rafaelateixeira@uol.com.br",
+        especialidade: "Hidráulica",
+        fone:  "43915400462",
+        nome: "Fernando Monteiro",
+        nota_media: "2.769230769230769",
+        prestador_id: "15"
+      },
+      {
+        avatar: "./Assets/colab6.jpg",
+        email: "felipe02@uol.com.br",
+        especialidade: "Pequenos Reparos",
+        fone:  "43953059476",
+        nome: "Ana Beatriz Martins",
+        nota_media: "2.875",
+        prestador_id: "16"
+      },
+      {
+        avatar: "./Assets/colab4.jpg",
+        email:  "xbarros@ig.com.br",
+        especialidade: "Acabamentos",
+        fone:  "43928514369",
+        nome: "Davi Duarte",
+        nota_media: "2.909090909090909",
+        prestador_id: "17"
+      },
+      {
+        avatar: "./Assets/colab7.jpg",
+        email:  "sophie93@yahoo.com.br",
+        especialidade: "Alvenaria e infraestrutra",
+        fone:  "43982106650",
+        nome: "Alice da Conceição",
+        nota_media: "2.6842105263157894",
+        prestador_id: "18"
+      },
+      {
+        avatar: "./Assets/colab5.jpg",
+        email:  "qmoraes@gmail.com",
+        especialidade: "Carpintaria",
+        fone:  "43941806626",
+        nome: "Benício Cardoso",
+        nota_media: "3.2142857142857144",
+        prestador_id: "19"
+      },
+      {
+        avatar: "./Assets/colab3.jpg",
+        email:  "alicia92@gmail.com",
+        especialidade: "Hidráulica",
+        fone:  "43917017639",
+        nome: "Laura Teixeira",
+        nota_media: "2.5454545454545454",
+        prestador_id: "20"
+      },  
+]

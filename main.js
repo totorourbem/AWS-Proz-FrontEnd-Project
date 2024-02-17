72c9100a4b12552e9933f36f40da4994d698d8f4
Ricardo Jeferson da Silva Francisco <122450316+totorourbem@users.noreply.github.com>
2024-02-17 18:50:32 -0300

    Upoload Cards
    página de colaboradores com codigo js ok (parte 1)

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const dados = [
       {
         avatar: "./Assets/colab8.jpg",
         email: "ida-rocha@gmail.com",
-        especialidade: "Alvenaria e infraestrutra",
+        especialidade: "Alvenaria e infraestrutura",
         fone: "43935360966",
         nome: "Vitor Santos",
         nota_media: "2.8461538461538463",
@@ -157,7 +157,7 @@ const dados = [
       {
         avatar: "./Assets/colab7.jpg",
         email:  "sophie93@yahoo.com.br",
-        especialidade: "Alvenaria e infraestrutra",
+        especialidade: "Alvenaria e infraestrutura",
         fone:  "43982106650",
         nome: "Alice da Conceição",
         nota_media: "2.6842105263157894",
@@ -180,32 +180,141 @@ const dados = [
         nome: "Laura Teixeira",
         nota_media: "2.5454545454545454",
         prestador_id: "20"
-      },  
+      },
+      {
+        avatar: "./Assets/colab2.jpg",
+        email:  "luiz-miguelbarbosa@gmail.com",
+        especialidade: "Acabamentos",
+        fone:  "43956940375",
+        nome: "Ana Beatriz Silva",
+        nota_media: "2.7058823529411766",
+        prestador_id: "21"
+      },
+      {
+        avatar: "./Assets/colab8.jpg",
+        email:  "peixotoenzo-gabriel@gmail.com",
+        especialidade: "Acabamentos",
+        fone:  "43969266893",
+        nome: "Luiz Henrique Fogaça",
+        nota_media: "3.8461538461538463",
+        prestador_id: "22"
+      },
+      {
+        avatar: "./Assets/colab3.jpg",
+        email:  "fernandesluiza@gmail.com",
+        especialidade: "Hidráulica",
+        fone:  "43976253973",
+        nome: "Laura Pinto",
+        nota_media: "2.72727272727272",
+        prestador_id: "23"
+      },
+      {
+        avatar: "./Assets/Colab1.jpg",
+        email:  "camila00@uol.com.br",
+        especialidade: "Pintura",
+        fone:  "43911702064",
+        nome: "Enrico Almeida",
+        nota_media: "3",
+        prestador_id: "24"
+      },
+      {
+        avatar: "./Assets/colab3.jpg",
+        email:  "beniciosales@bol.com.br",
+        especialidade: "Pequenos Reparos",
+        fone:  "43964705161",
+        nome: "Cecília Porto",
+        nota_media: "2.9",
+        prestador_id: "25"
+      },{
+        avatar: "./Assets/colab2.jpg",
+        email:  "luiz-henriquerocha@ig.com.br",
+        especialidade: "Hidráulica",
+        fone:  "43927587663",
+        nome: "Ana Alves",
+        nota_media: "2.8",
+        prestador_id: "26"
+      },
+      {
+        avatar: "./Assets/colab5.jpg",
+        email:  "marcelo61@ig.com.br",
+        especialidade: "Pequenos Reparos",
+        fone:  "43920697847",
+        nome: "Henrique Moreira",
+        nota_media: "3.4444444444444446",
+        prestador_id: "27"
+      },
+      {
+        avatar: "./Assets/Colab1.jpg",
+        email:  "marianunes@bol.com.br",
+        especialidade: "Carpintaria",
+        fone:  "43921210255",
+        nome: "Thales Moraes",
+        nota_media: "2.5",
+        prestador_id: "28"
+      },
+      {
+        avatar: "./Assets/colab4.jpg",
+        email:  "ysilva@yahoo.com.br",
+        especialidade: "Hidráulica",
+        fone:  "43917410210",
+        nome: "Kevin das Neves",
+        nota_media: "3.3333333333333335",
+        prestador_id: "29"
+      },
+      {
+        avatar: "./Assets/colab8.jpg",
+        email:  "da-cruzelisa@uol.com.br",
+        especialidade: "Hidráulica",
+        fone:  "43942578854",
+        nome: "Vinicius da Cruz",
+        nota_media: "2.8",
+        prestador_id: "30"
+      }    
 ]
 
-dadosSelecionados = [];
-
 let valorSelecionado = document.getElementById('especialidade-selecionada');
+let gridCardsDiv = document.getElementById('grid-cards')
 
   // Adicionando um ouvinte de evento para o evento 'change'
   valorSelecionado.addEventListener('change', function() {
     // Obtendo o valor selecionado
-    let valorSelecionado = valorSelecionado.value;
+    let valorSelecionado2 = valorSelecionado.value;
+    gridCardsDiv.innerHTML = '';
     // Percorrendo o array de dados
-  for (var i = 0; i <= dados.length; i++) {
-    if (dados[i]['especialidade'] === "Carpintaria") {
-      dadosSelecionados.push(dados[i]);
+    for (let i = 0; i < dados.length; i++) {
+      if (dados[i]['especialidade'] === valorSelecionado2) {
+        // Criando elementos HTML para o card
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        cardDiv.style.width = '48%';
+        cardDiv.style.margin = '2px';
+
+        cardDiv.innerHTML = `
+          <div class="row g-0">
+            <div class="col-md-4 d-flex align-items-center">
+              <img src="${dados[i]['avatar']}" class="card-img-top card-img-bottom" alt="Avatar" style="height: 90%;">
+            </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h6 class="card-name"><strong>Nome:</strong> <i><em>${dados[i]['nome']}</em></i> </h6>
+              <p class="card-especialidade"><b><strong>Especialidade:</strong></b> <i><em>${dados[i]['especialidade']}</em></i> </p>
+              <p class="card-email"><b><strong>Email:</strong></b> <i><em>${dados[i]['email']}</em></i></p>
+              <p class="card-fone"><b><strong>Telefone:</strong></b> <i><em>${dados[i]['fone']}</em></i></p>
+              <p class="card-media"><b><strong>Nota Média:</strong></b> <i><em>${dados[i]['nota_media']}</em></i></p>
+            </div> 
+          </div>
+          <div class="d-grid">
+            <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#detalhesItem">
+              <i class="fas fa-chevron-down" value="${dados[i]['prestador_id}']}">clique para ver mais detalhes</i>
+            </button>
+          </div>    
+          </div>
+        ` ;
+
+        // Adicionando o card à div de grid
+        gridCardsDiv.appendChild(cardDiv);
+        }  
+      }
     }
-  }})
-
-
-// Lógica possivel com for
-// dadosSelecionados = [];
-
-// for (var i = 0; i < dados.length; i++) {
-//   if (dados[i]['especialidade'] === "Carpintaria") {
-//     dadosSelecionados.push(dados[i]);
-//     }
-// }
-
-// console.log(dadosSelecionados)  
+  )      
+  
